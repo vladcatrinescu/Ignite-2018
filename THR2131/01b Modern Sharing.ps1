a5148a7dc1f2ad081f48359e2397d0122f3608a4
vladcatrinescu <vlad@vnext.solutions>
2018-09-21 18:14:06 -0400

    Final Demo for THR2131

--- THR2131/01b Modern Sharing.ps1
+++ THR2131/01b Modern Sharing.ps1
@@ -1,8 +1,14 @@
 $SiteCollections  = Get-SPOSite -Limit All
 foreach ($site in $SiteCollections)
 {
-	$ExternalUserswithModern += Get-SPOUser -Limit All -Site $site.Url | Where {$_.LoginName -like "*urn:spo:guest*" -or $_.LoginName -like "*#ext#*"} | Select DisplayName,LoginName,@{Name = "Url" ; Expression = { $site.url }
-	}
+    try 
+    {
+	$ExternalUserswithModern += Get-SPOUser -Limit All -Site $site.Url | Where {$_.LoginName -like "*urn:spo:guest*" -or $_.LoginName -like "*#ext#*"} | Select DisplayName,LoginName,@{Name = "Url" ; Expression = { $site.url }}
+    }
+    catch
+    { 
+    Write-Host "Could not get user from the following URL! Make sure you are a SC Admin of it! " $site.Url
+    }
 }
 
 $ExternalUserswithModern | Export-Csv -Path "C:\PowerShell\ExternalUsersPerSCWithModern.csv" -NoTypeInformation
